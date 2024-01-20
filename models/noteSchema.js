fd5698c220fe4d9ca2e35e29aab8f6c5181bccf7
shubhambargal2002 <shubhambargal2002@gmail.com>
2024-01-20 14:21:19 +0530

    added model of noteSchema

---
+++ models/noteSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tag: {
+      type: String,
+      default: "General",
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("NOTE", noteSchema);
+module.exports = Note;
